e626a5442aaa4065113e5ab1e6e3da0d4d904d0f
version-updater <vu.bot@users.noreply.github.com>
2026-07-02 15:33:33 +0000

    Update version to 0.0.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 
 project = 'biosim-schema'
 copyright = '2026, CCPBioSim'
-release = '0.0.4'
+release = '0.0.5'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
